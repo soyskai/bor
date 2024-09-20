3d6da97f2dee8b1e851f1a5c89da7f9b9d96d0b5
SoyNeo12 <130333212+SoyNeo12@users.noreply.github.com>
2024-09-20 18:37:45 -0300

    index.js

--- ledigoholagoodbye/index.js
+++ ledigoholagoodbye/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const { haxballBot } = require('./examples/script');
-const { discordBot } = require('./examples/bot');
 
 const app = express();
 
 app.get('/start/script1', (req, res) => {
     haxballBot;
     res.send('script1.js iniciado');
-});
-
-app.get('/start/script2', (req, res) => {
-    discordBot;
-    res.send('script2.js iniciado');
 });
 
 app.listen(3000, () => {
